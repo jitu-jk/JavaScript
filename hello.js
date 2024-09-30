9f03293d42762e55ee4f7a998ab50109c92c750a
jitu-jk <jitesh62684@gmail.com>
2024-09-30 22:43:20 +0530

    hello world

---
+++ hello.js
@@ -0,0 +1,12 @@
+
+ console.log("hello world"); //print on console
+
+ document.write("hello world"); // print on browser
+
+ window.alert("hello world");  //print with alert message
+
+ alert("hello world");      // print with alert msg without window keyword
+
+ window.print("hello world"); //print with external file 
+
+
